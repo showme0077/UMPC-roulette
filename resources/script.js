dabcab54be0aa0f5a0dc86bd987ef09f6b6475bf
YoungJun Yoo <showme0077@gmail.com>
2023-09-22 09:46:29 +0000

    룰렛 대신 로딩바로 변경, 추첨인원, 제외 값 스크립트 추가

--- resources/script.js
+++ resources/script.js
@@ -1,61 +1,41 @@
-let spinning = false;
+function showLoadingAndResult() {
+    const maxNumber = parseInt(document.getElementById("max-number").value);
+    const drawCount = document.getElementById("draw-count").value ? parseInt(document.getElementById("draw-count").value) : 1;
+    let excludeValues = document.getElementById("exclude-values").value;
 
-function drawNumbers(maxNumber) {
-    const svgNamespace = "http://www.w3.org/2000/svg";
-    const numbersContainer = document.getElementById("numbers");
-    const sectorsContainer = document.getElementById("sectors");
-    numbersContainer.innerHTML = '';  // Clear previous numbers
-    sectorsContainer.innerHTML = ''; // Clear previous sectors
+    excludeValues = excludeValues ? excludeValues.split(',').map(val => parseInt(val)).filter(val => !isNaN(val) && val <= maxNumber) : [];
 
-    const colors = ['#FFDDDD', '#DDEEFF', '#DDFFDD', '#FFFFDD', '#FFDDBB', '#DDBBFF']; // Example colors
-
-    for (let i = 0; i < maxNumber; i++) {
-        const startAngle = i * (360 / maxNumber);
-        const endAngle = (i + 1) * (360 / maxNumber);
-        const x1 = 100 + 90 * Math.cos(startAngle * Math.PI / 180);
-        const y1 = 100 + 90 * Math.sin(startAngle * Math.PI / 180);
-        const x2 = 100 + 90 * Math.cos(endAngle * Math.PI / 180);
-        const y2 = 100 + 90 * Math.sin(endAngle * Math.PI / 180);
-
-        // Draw sector
-        const sector = document.createElementNS(svgNamespace, "path");
-        sector.setAttribute("d", `M 100 100 L ${x1} ${y1} A 90 90 0 0 1 ${x2} ${y2} Z`);
-        sector.setAttribute("fill", colors[i % colors.length]);
-        sectorsContainer.appendChild(sector);
-
-        // Draw number
-        const angle = (i + 0.5) * (360 / maxNumber);
-        const x = 100 + 70 * Math.cos(angle * Math.PI / 180);
-        const y = 100 + 70 * Math.sin(angle * Math.PI / 180);
-
-        const numberElement = document.createElementNS(svgNamespace, "text");
-        numberElement.setAttribute("x", x);
-        numberElement.setAttribute("y", y);
-        numberElement.setAttribute("text-anchor", "middle");
-        numberElement.setAttribute("dominant-baseline", "middle");
-        numberElement.textContent = (i + 1).toString();
-        numbersContainer.appendChild(numberElement);
-    }
-}
-
-function spinRoulette() {
-    if (spinning) return;
-    spinning = true;
-
-    const maxNumber = parseInt(document.getElementById('max-number').value);
-    const randomResult = Math.floor(Math.random() * maxNumber) + 1;
-
-    const spins = Math.floor(Math.random() * 10) + 5;
-    const rotation = spins * 360 - (randomResult - 0.5) * (360 / maxNumber);
-
-    const rouletteSvg = document.getElementById('roulette-svg');
-    rouletteSvg.style.transform = `rotate(${rotation}deg)`;
-
+    // 결과를 얻기 전 로딩 애니메이션 보여주기
+    document.querySelector(".loading-animation").style.display = "block";
+    document.querySelector("#result").style.display = "none";
     setTimeout(() => {
-        document.getElementById('result').textContent = `결과: ${randomResult}`;
-        spinning = false;
-    }, 2000);
+        const results = drawNumbers(maxNumber, drawCount, excludeValues);
+        document.getElementById("result").innerText = "축하합니다!! 당첨번호는: " + results.join(', ') + "입니다.";
+        document.querySelector(".loading-animation").style.display = "none";
+        document.querySelector("#result").style.display = "block";
+    }, 3500);  // 3.5초 후 결과 보여주기
 }
 
-// Initial draw for default value of 10
-drawNumbers(10);
+function drawNumbers(maxNumber, drawCount, excludeValues) {
+    const availableNumbers = Array.from({length: maxNumber}, (_, i) => i + 1).filter(val => !excludeValues.includes(val));
+    const results = [];
+    for (let i = 0; i < drawCount; i++) {
+        if (availableNumbers.length === 0) break;
+        const randomIndex = Math.floor(Math.random() * availableNumbers.length);
+        results.push(availableNumbers[randomIndex]);
+        availableNumbers.splice(randomIndex, 1);
+    }
+    return results;
+}
+
+window.onload = function() {
+    const maxNumber = parseInt(document.getElementById("max-number").value);
+    const selectBox = document.getElementById("draw-count");
+    for(let i = 1; i <= maxNumber; i++) {
+        const option = document.createElement('option');
+        option.value = i;
+        option.innerText = i;
+        selectBox.appendChild(option);
+    }
+    selectBox.value = "1";  // Set default draw count to 1
+}
