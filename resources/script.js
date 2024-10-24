d1a3a31ced34c9d1b6bfdfe3dab3a57afe499cd6
YoungJun Yoo <showme0077@gmail.com>
2024-10-24 11:30:14 +0000

    인원보다 뽑기인원이 많은경우 예외처리

--- resources/script.js
+++ resources/script.js
@@ -1,19 +1,24 @@
 function showLoadingAndResult() {
     const maxNumber = parseInt(document.getElementById("max-number").value);
     const drawCount = document.getElementById("draw-count").value ? parseInt(document.getElementById("draw-count").value) : 1;
-    let excludeValues = document.getElementById("exclude-values").value;
+    if(maxNumber < drawCount) {
+        document.getElementById("result").innerText = "잘못된선택";
+    } else{
+        let excludeValues = document.getElementById("exclude-values").value;
 
-    excludeValues = excludeValues ? excludeValues.split(',').map(val => parseInt(val)).filter(val => !isNaN(val) && val <= maxNumber) : [];
+        excludeValues = excludeValues ? excludeValues.split(',').map(val => parseInt(val)).filter(val => !isNaN(val) && val <= maxNumber) : [];
 
-    // 결과를 얻기 전 로딩 애니메이션 보여주기
-    document.querySelector(".loading-animation").style.display = "block";
-    document.querySelector("#result").style.display = "none";
-    setTimeout(() => {
-        const results = drawNumbers(maxNumber, drawCount, excludeValues);
-        document.getElementById("result").innerText = "축하합니다!! 당첨번호는: " + results.join(', ') + "입니다.";
-        document.querySelector(".loading-animation").style.display = "none";
-        document.querySelector("#result").style.display = "block";
-    }, 3500);  // 3.5초 후 결과 보여주기
+        // 결과를 얻기 전 로딩 애니메이션 보여주기
+        document.querySelector(".loading-animation").style.display = "block";
+        document.querySelector("#result").style.display = "none";
+        setTimeout(() => {
+            const results = drawNumbers(maxNumber, drawCount, excludeValues);
+            document.getElementById("result").innerText = "축하합니다!! 당첨번호는: " + results.join(', ') + "입니다.";
+            document.querySelector(".loading-animation").style.display = "none";
+            document.querySelector("#result").style.display = "block";
+        }, 3500);  // 3.5초 후 결과 보여주기
+    }
+    
 }
 
 function drawNumbers(maxNumber, drawCount, excludeValues) {
@@ -26,6 +31,10 @@ function drawNumbers(maxNumber, drawCount, excludeValues) {
         availableNumbers.splice(randomIndex, 1);
     }
     return results;
+}
+
+function order(results){
+
 }
 
 window.onload = function() {
